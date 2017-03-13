e392c89259aeb65caca086e3b5b540b64dfa7532
Kevin Gin <kevinagin@gmail.com>
2017-03-13 10:29:41 -0700

    prints board

---
+++ src/tictactoe.js
@@ -0,0 +1,31 @@
+var prompt = require('prompt');
+
+// console.log('hello!')
+
+// console.log('1 | 2 | 3')
+// console.log('_ | _ | _')
+// console.log('1 | 2 | 3')
+// console.log('_ | _ | _')
+// console.log('1 | 2 | 3')
+
+// console.log('X | X | X')
+// console.log('_ | _ | _')
+// console.log('O | O | X')
+// console.log('_ | _ | _')
+// console.log('X | O | X')
+
+console.log('  |   |  ')
+console.log('_ | _ | _')
+console.log('  |   |  ')
+console.log('_ | _ | _')
+console.log('  |   |  ')
+
+
+
+
+
+// prompt.start();
+
+// prompt.get(['yo', 'hi'], function(err,data){
+// 	console.log(data)
+// })
