0b355a1fb56ec682da7c75f7079557162eb94ce8
Kevin Gin <kevinagin@gmail.com>
2017-03-13 12:25:42 -0700

    (MVP) displays winner, handles invalid user input, doesnt allow overwrite spaces

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -25,12 +25,14 @@ var Board = function() {
 };
 
 Board.prototype.insert = function(col, row) {
+	console.log('insert called. turn =>', this.turn)
+
 	this.storage[col][row] = this.turn  // (row-1) because user doesn't see 0
 	this.turn === 'X' ? this.turn = 'O' : this.turn = 'X'
 }
 
 //only check most recent entry, not entire board
-Board.prototype.check = function(col,row) {
+Board.prototype.success = function(col,row) {
 	return this.rowSuccess(row) || this.columnSuccess(col) || this.diagonalSuccess();
 }
 
@@ -86,39 +88,75 @@ Board.prototype.displayCol = function(col) {
 	console.log('  '+ col + '      ' + displayZero +   ' | ' + displayOne + ' | ' + displayTwo)
 }
 
+function inputIsValid(col,row) {
+	return rowIsValid(row) && columnIsValid(col);
+}
+
+function rowIsValid(row) {
+	var row = parseInt(row);
+	return row === 1 || row === 2 || row === 3;
+}
+
+function columnIsValid(col) {
+	// check type first so string method doesn't throw error
+	if (typeof col !== 'string') {
+		return false;
+	}
+	var col = col.toUpperCase();
+	return col === 'A' || col === 'B' || col === 'C'
+}
+
+function spotTaken(col,row,board) {
+	return !!(board.storage[col][row])
+}
 
 
-// board.insert('A', 2, 'X')
-// board.insert('A', 1, 'X')
-// board.insert('B', 0, 'O')
-// board.insert('A', 0, 'O')
-// board.insert('C', 0, 'O')
-
-
-
-
-
-var board = new Board();
-
-prompt.start()
-
+// has closure over both board and prompt
+function setPromptMessage() {
+	var messageEnding = board.turn === 'X' ? '1' : '2';
+	prompt.message = 'Your move, Player ' + messageEnding;
+}
 
 
 function playGame(board) {
 	board.display();
+	setPromptMessage();
 	prompt.get(['column', 'row'], function(err,data) {
-		console.log('got info')
-		board.insert(data.column, data.row - 1);
+		var col = data.column;
+		var row = data.row;
+		// handle invalid user input
+		if (!inputIsValid(col,row)){
+			console.log('You\'re nuts! Please enter a valid input. A-C for columns, 1-3 for rows')
+			return playGame(board); 
+		}
+		var row = data.row-1  // we don't display row 0 to users
+		var col = col.toUpperCase();
+		// handle spot already taken
+		if (spotTaken(col, row, board)) {
+			console.log('Spot already taken, please try again.')
+			return playGame(board);
+		}
+		board.insert(col, row);
+		// handle player wins
+		if (board.success(col,row)) {
+			var playerId = board.turn === 'X' ? '2' : '1';
+			board.display();
+			return console.log('Congratulations! Player ' + playerId + ' wins!');
+		}
+		// recurse
 		playGame(board);
 	})
 }
 
-
+var board = new Board();
+prompt.start();
+setPromptMessage();
 playGame(board);
 
 
 
 
+// Check to make sure spot isn't taken
 
 
 
